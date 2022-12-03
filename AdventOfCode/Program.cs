1af38c8ee35a35eb7777fccdb157019a99c3bfa7
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2022-12-02 19:27:38 -0600

    Day1 Completed

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,77 +6,44 @@ namespace AdventOfCode
 {
     internal class Program
     {
-        // cookie value comes from the advent of code website when you are logged in. 
-        private const string _cookieFile = ".cookie"; 
-        private static Uri _uri = new Uri("https://adventofcode.com");
 
         static void Main(string[] args)
         {
-            Day1Processing(); 
+            //Day1Processing(); 
+            Day2Processing();
         }
 
         static void Day1Processing()
         {
-            string dataPath = "Day1Data.txt";
-            using var reader = new StreamReader(dataPath);
-            string data = reader.ReadToEnd();
-
-            foreach (var i in data)
-            {
-                Console.WriteLine(i);
-            }
-            //Console.WriteLine(data[0]);
-            //string line;
-
-
-
+            int elfCalories = 0;
             
+            string[] lines = File.ReadAllLines("Day1Data.txt");
+            List<int> submissionFileList = new List<int>();
+                        
+            foreach (string line in lines)
+            {    
+                if (line != "")
+                {
+                    elfCalories = elfCalories + Int32.Parse(line);
+                }
+                else if (line == "")
+                {
+                    submissionFileList.Add(elfCalories);
+                    elfCalories = 0;
+                }
+            }
+
+            submissionFileList.Sort();
+
+            foreach (int line in submissionFileList)
+            {   
+                Console.WriteLine(line);
+            }
         }
-        /// <summary>
-        /// Get the input from the input url depending on day, year and the cookie. Writes to a file in bin. 
-        /// </summary>
-        /// <param name="day"></param>
-        /// <param name="year"></param>
-        /// <param name="cookie"></param>
-        /// <param name="filename"></param>
-        /// <returns></returns>
-        static async Task GetInput(int day, int year, string cookie, string filename)
-        {
-            var uri = new Uri("https://adventofcode.com");
-                var cookies = new CookieContainer();
-                cookies.Add(uri, new System.Net.Cookie("session", cookie));
-                using var file = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
-                using var handler = new HttpClientHandler() { CookieContainer = cookies };
-                using var client = new HttpClient(handler) { BaseAddress = uri };
-                using var response = await client.GetAsync($"/{year}/day/{day}/input");
-                using var stream = await response.Content.ReadAsStreamAsync();
-                await stream.CopyToAsync(file);
-        }
 
-        static async Task<byte[]> GetInput(int day, int year, string cookie)
+        static void Day2Processing()
         {
 
-            using MemoryStream ms = new(); 
-            var cookies = new CookieContainer();
-            cookies.Add(_uri, new System.Net.Cookie("session", cookie));
-            using var handler = new HttpClientHandler() { CookieContainer = cookies };
-            using var client = new HttpClient(handler) { BaseAddress = _uri };
-            using var response = await client.GetAsync($"/{year}/day/{day}/input");
-            using var stream = await response.Content.ReadAsStreamAsync();
-
-            await stream.CopyToAsync(ms);
-
-            return ms.ToArray();
-        }
-        /// <summary>
-        /// Loads a file called .cookie that has a unique key that allows you to get your own
-        /// dataset from the advent of code website. 
-        /// </summary>
-        /// <returns></returns>
-        private static string ReadCookie()
-        {
-            using StreamReader reader= new StreamReader(_cookieFile);
-            return reader.ReadToEnd(); 
         }
     }
 }
